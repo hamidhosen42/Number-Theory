883591e18f25fd18ad6013a0150c1c3b9d55230f
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2021-10-02 22:02:13 +0600

    Update check prime number.cpp

--- check prime number.cpp
+++ check prime number.cpp
@@ -3,7 +3,7 @@ bool is_prime(ll n){
     if(n%2==0 or n%3==0) return 0;
 
     for(ll i=5; i*i<=n; i+=6){
-        cout<<i<<" ";
+        //cout<<i<<" ";
         if(n%i==0 or n%(i+2)==0){
             return 0;
         }
